d192a5d37c3baa67bc0975cc3c3164378720c66b
Paulino Marié <paulinodjm@hotmail.com>
2015-02-01 00:03:24 +0100

    Vire quelques using inutiles.

--- TheGame/TextureManager.cs
+++ TheGame/TextureManager.cs
@@ -3,9 +3,6 @@ using SFML.Graphics;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TheGame
 {
